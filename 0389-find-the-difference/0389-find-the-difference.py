3d65b5f5d0dddd2cce2884300444cbc5269096e7
akshaydeepakm44 <141931266+akshaydeepakm44@users.noreply.github.com>
2025-01-24 23:33:53 +0530

    Time: 8 ms (18.56%), Space: 12.6 MB (14.42%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        for char in t:
+            if char not in s:
+                return char
+            else:
+                s = s.replace(char, '', 1)
