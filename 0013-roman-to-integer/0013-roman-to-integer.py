643d1f2241f50e3213604cd762d6d68286c7d0b0
akshaydeepakm44 <141931266+akshaydeepakm44@users.noreply.github.com>
2025-07-10 15:43:35 +0530

    Time: 8 ms (26.97%), Space: 17.7 MB (64.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,25 +1,26 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        roman_numerals = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # Step 1: Map of Roman numerals
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
         }
         
-        total = 0
-        prev_value = 0
+        total = 0  # Final result
         
-        for char in reversed(s):
-            value = roman_numerals[char]
-        
-            if value < prev_value:
+        # Step 2: Loop through the string
+        for i in range(len(s)):
+            value = roman[s[i]]  # Current Roman character's value
+            
+            # Step 3: Check if next character is larger (subtraction case)
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
                 total -= value
             else:
                 total += value
-            
-            prev_value = value
         
         return total
-
