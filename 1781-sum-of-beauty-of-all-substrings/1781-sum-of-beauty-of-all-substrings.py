49a2299731dcd5f68b73bf91674bf4fe2103f676
akshaydeepakm44 <141931266+akshaydeepakm44@users.noreply.github.com>
2025-07-10 18:09:32 +0530

    Time: 1315 ms (77.43%), Space: 17.7 MB (56.81%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -8,4 +8,12 @@ class Solution:
                 values  = freq.values()
                 total += max(values) - min(values)
         return total 
- 
+#   total = 0                                   brute approch
+#         for i in range(len(s)):
+#             for j in range(i + 1, len(s) + 1):
+#                 sub = s[i:j]
+#                 freq = {}
+#                 for ch in sub:
+#                     freq[ch] = freq.get(ch, 0) + 1
+#                 total += max(freq.values()) - min(freq.values())
+#         return total
